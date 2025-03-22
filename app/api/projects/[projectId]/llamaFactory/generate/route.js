aecb19d56f562e0b888d9fcff0e1d1b505be1a24
ConardLi <1009903985@qq.com>
2025-03-22 22:23:46 +0800

    fix: dataset_info.json

--- app/api/projects/[projectId]/llamaFactory/generate/route.js
+++ app/api/projects/[projectId]/llamaFactory/generate/route.js
@@ -30,7 +30,7 @@ export async function POST(request, { params }) {
 
         // 创建 dataset_info.json 配置
         const config = {
-            [`${projectId}Alpaca`]: {
+            [`[Easy Dataset] [${projectId}] Alpaca`]: {
                 file_name: 'alpaca.json',
                 columns: {
                     prompt: 'instruction',
@@ -39,7 +39,7 @@ export async function POST(request, { params }) {
                     system: 'system'
                 }
             },
-            [`${projectId}ShareGPT`]: {
+            [`[Easy Dataset] [${projectId}] ShareGPT`]: {
                 file_name: 'sharegpt.json',
                 formatting: 'sharegpt',
                 columns: {
