02e39bbb4da23c25a42e31385cbcf5990c7d7cf0
kakachake <282281884@qq.com>
2025-03-28 22:05:15 +0800

    chore: 样式问题修复

--- .prettierrc.js
+++ .prettierrc.js
@@ -1,13 +1,13 @@
 module.exports = {
-  "semi": true,
-  "trailingComma": "none",
-  "singleQuote": true,
-  "tabWidth": 2,
-  "useTabs": false,
-  "bracketSpacing": true,
-  "arrowParens": "avoid",
-  "proseWrap": "preserve",
-  "jsxBracketSameLine": true,
-  "printWidth": 120,
-  "endOfLine": "auto"
+  semi: true,
+  trailingComma: 'none',
+  singleQuote: true,
+  tabWidth: 2,
+  useTabs: false,
+  bracketSpacing: true,
+  arrowParens: 'avoid',
+  proseWrap: 'preserve',
+  jsxBracketSameLine: true,
+  printWidth: 120,
+  endOfLine: 'auto'
 };
