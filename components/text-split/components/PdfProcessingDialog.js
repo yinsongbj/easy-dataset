6890ad61b72c1f1a03c471c2ded1edd2b5b00f3b
jh_personal@163.com <jh_personal@163.com>
2025-03-29 20:37:20 +0800

    国际化内容优化，暂时隐藏视觉大模型方案

--- components/text-split/components/PdfProcessingDialog.js
+++ components/text-split/components/PdfProcessingDialog.js
@@ -27,24 +27,19 @@ export default function PdfProcessingDialog({
       </DialogTitle>
       <DialogContent>
       <DialogContentText id="delete-dialog-description">
-          请选择PDF文件处理方式：
+        {t('textSplit.selectPdfProcessingStrategy')}
         </DialogContentText>
         <FormControl component="fieldset">
           <RadioGroup aria-label="gender" name="gender1" value={value} onChange={onRadioChange}>
             <FormControlLabel value="default" control={<Radio />} label={
               <div style={{ display: 'flex', alignItems: 'center' }}>
-                <span>默认</span>
-                <Tooltip disableFocusListener title="使用内置PDF解析策略"><HelpOutlineIcon sx={{ ml: 35 }} /></Tooltip>
+                <span>{t('textSplit.pdfProcessingStrategyDefault')}</span>
+                <Tooltip disableFocusListener title={t('textSplit.pdfProcessingStrategyDefaultHelper')}><HelpOutlineIcon sx={{ ml: 35 }} /></Tooltip>
               </div>} />
             <FormControlLabel value="mineru" control={<Radio />} disabled={!isMinerUEnabled} label={
               <div style={{ display: 'flex', alignItems: 'center' }}>
                 <span>MinerU</span>
-                <Tooltip disableFocusListener title="使用MinerU API解析，请先配置MinerU API Token"><HelpOutlineIcon sx={{ ml: 32 }} /></Tooltip>
-              </div>} />
-            <FormControlLabel value="vision" control={<Radio />} label={
-              <div style={{ display: 'flex', alignItems: 'center' }}>
-                <span>自定义视觉模型</span>
-                <Tooltip disableFocusListener title="使用自定义视觉模型解析"><HelpOutlineIcon sx={{ ml: 25 }} /></Tooltip>
+                <Tooltip disableFocusListener title={t('textSplit.pdfProcessingStrategyMinerUHelper')}><HelpOutlineIcon sx={{ ml: 32 }} /></Tooltip>
               </div>} />
           </RadioGroup>
         </FormControl>
